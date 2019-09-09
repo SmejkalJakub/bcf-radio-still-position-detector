d349633a36fadf8ef8c63d1714bd34251ec89791
Jakub Smejkal <smejkal.jakub99@gmail.com>
2019-09-09 15:04:58 +0200

    Update application.c

--- app/application.c
+++ app/application.c
@@ -1,6 +1,8 @@
 #include <application.h>
 
 #define RADIO_DELAY 10000
+#define START_DELAY 600
+
 
 #define BATTERY_UPDATE_INTERVAL (60 * 60 * 1000)
 
@@ -15,8 +17,7 @@ bc_button_t button;
 bc_lis2dh12_t acc;
 bc_lis2dh12_result_g_t result;
 
-bc_lis2dh12_alarm_t alarm;
-
+float magnitude;
 
 bc_tick_t startSeconds = 0;
 bc_tick_t endSeconds = 0;
@@ -27,43 +28,55 @@ void lis2_event_handler(bc_lis2dh12_t *self, bc_lis2dh12_event_t event, void *ev
 {
     float holdTime;
 
-    if (event == BC_LIS2DH12_EVENT_UPDATE)
+    if (event == BC_LIS2DH12_EVENT_UPDATE && playing)
     {
-        bc_lis2dh12_get_result_g(&acc, &result);
-    }
-    else if (event == BC_LIS2DH12_EVENT_ALARM && playing)
-    {
-        endSeconds = bc_tick_get();
-        playing = false;
         static bc_tick_t radio_delay = 0;
-        bc_led_set_mode(&led, BC_LED_MODE_OFF);
-        if (bc_tick_get() >= radio_delay)
-        {
-            // Make longer pulse when transmitting
-            bc_led_pulse(&led, 100);
 
-            holdTime = ((endSeconds - startSeconds) / (float)1000);
+        bc_lis2dh12_get_result_g(&acc, &result);
 
-            bc_radio_pub_float("hold-time", &holdTime);
-
-            radio_delay = bc_tick_get() + RADIO_DELAY;
-
-        }
-    }
-    else
-    {
+        magnitude = pow(result.x_axis, 2) + pow(result.y_axis, 2) + pow(result.z_axis, 2);
+        magnitude = sqrt(magnitude);
         
+
+        if(magnitude > 1.19 || magnitude < 0.95)
+        {
+            endSeconds = bc_tick_get();
+            playing = false;
+
+            bc_lis2dh12_set_update_interval(&acc, BC_TICK_INFINITY);
+            bc_led_set_mode(&led, BC_LED_MODE_OFF);
+
+            if (bc_tick_get() >= radio_delay)
+            {
+                // Make longer pulse when transmitting
+                bc_led_pulse(&led, 100);
+
+                holdTime = ((endSeconds - startSeconds) / (float)1000);
+
+                bc_radio_pub_float("hold-time", &holdTime);
+
+                radio_delay = bc_tick_get() + RADIO_DELAY;
+            }
+        }
     }
 }
 
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
 { 
+    static bc_tick_t start_delay = 0;
     if (event == BC_BUTTON_EVENT_PRESS)
     {
+        while(bc_tick_get() >= start_delay)
+        {
+            start_delay = bc_tick_get() + START_DELAY;
+        }
+
+        bc_lis2dh12_set_update_interval(&acc, 40);
+
         playing = true;
         startSeconds = bc_tick_get();
+
         bc_led_set_mode(&led, BC_LED_MODE_ON);
-        bc_radio_pub_event_count(BC_RADIO_PUB_EVENT_PUSH_BUTTON, NULL);
     }
 
 }
@@ -113,13 +126,5 @@ void application_init(void)
 
     bc_lis2dh12_init(&acc, BC_I2C_I2C0, 0x19);
     bc_lis2dh12_set_event_handler(&acc, lis2_event_handler, NULL);
-
-    memset(&alarm, 0, sizeof(alarm));
-
-    // Activate alarm when axis Z is above 0.25 or below -0.25 g
-    alarm.z_high = true;
-    alarm.threshold = 0.7;
-    alarm.duration = 0;
-
-    bc_lis2dh12_set_alarm(&acc, &alarm);
+    bc_lis2dh12_set_update_interval(&acc, BC_TICK_INFINITY);
 }
